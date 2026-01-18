121ce490777caa06a238846a4ec7cb54bc49edc7
Your Name <you@example.com>
2026-01-18 19:15:47 +0530

    feat - finalised deduping and storing in database

--- processing/common/mongo_client.py
+++ processing/common/mongo_client.py
@@ -5,6 +5,7 @@ DB_NAME = "proportion_db_v1"
 
 RAW_COLLECTION = "articles_raw"
 EMBEDDED_COLLECTION = "articles_embedded"
+DEDUP_GROUPS_COLLECTION = "semantic_dedup_groups"
 
 
 def get_raw_articles_collection():
@@ -15,3 +16,8 @@ def get_raw_articles_collection():
 def get_embedded_articles_collection():
     client = MongoClient(MONGO_URI)
     return client[DB_NAME][EMBEDDED_COLLECTION]
+
+
+def get_semantic_dedup_groups_collection():
+    client = MongoClient(MONGO_URI)
+    return client[DB_NAME][DEDUP_GROUPS_COLLECTION]
